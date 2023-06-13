43beed25031d0839739c589780455f258e331729
Jason W. Bacon <bacon4000@gmail.com>
2023-06-13 14:55:22 -0500

    Add cave-man-install.sh with qmake6

---
+++ cave-man-install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+##########################################################################
+#
+#   Perform a cave-man install for development and testing purposes.
+#   For production use, this software should be installed via a package
+#   manager such as Debian packages, FreeBSD ports, MacPorts, pkgsrc, etc.
+#       
+#   History:
+#   Date        Name        Modification
+#   2021-07-12  Jason Bacon Begin
+##########################################################################
+
+# Default to ../local if LOCALBASE is not set
+: ${LOCALBASE:=../local}
+
+# OS-dependent tricks
+# Set rpath to avoid picking up libs installed by package managers in
+# /usr/local/lib, etc.
+case $(uname) in
+*)
+    export CFLAGS="-Wall -g -O"
+    LIBDIR=$(realpath $LOCALBASE/lib)
+    LOCALBASE=$(realpath $LOCALBASE)
+    PREFIX=$LOCALBASE
+    export PREFIX LOCALBASE
+    export LDFLAGS="-L. -L$LIBDIR -Wl,-rpath,$LIBDIR:/usr/lib:/lib"
+    export PATH=${PATH}:/usr/pkg/qt6/bin
+    ;;
+
+esac
+
+qmake6
+make clean install
